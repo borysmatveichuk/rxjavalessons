8f18ef9215f99526f06b5ea15257833c81948184
borysmatveichuk <boris.matveychuck@gmail.com>
2019-02-10 23:06:05 +0200

    Scan

--- src/main/java/ScanTest.java
+++ src/main/java/ScanTest.java
@@ -2,6 +2,7 @@ import io.reactivex.Observable;
 
 public class ScanTest {
     private static String result = "";
+    private static Integer resultInt = 0;
 
     public static void main(String[] args) {
         String[] letters = {"a", "B", "c", "d"};
@@ -10,5 +11,20 @@ public class ScanTest {
                 .subscribe(total -> result += total.toString());
 
         System.out.println(result);
+
+        Integer[] numbers = {1, 2, 3, 4, 5};
+        Observable.fromArray(numbers)
+                .scan((integer, integer2) -> {
+
+                    System.out.println("integer " + integer);
+                    System.out.println("integer2 " + integer2);
+                    return integer + integer2;
+                })
+                .subscribe(total -> {
+                    resultInt = total;
+                    System.out.println("total " + total);
+                });
+
+        System.out.println("total " + resultInt);
     }
 }
